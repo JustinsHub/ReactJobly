476cccb2ffb772884f964714047f8ffbc5218057
Justin Z <contactjustinzamora@gmail.com>
2021-02-23 23:31:09 -0800

    Along with PrivateRoute component implmented for users that are logged in

--- frontend/src/components/CompanyCard.js
+++ frontend/src/components/CompanyCard.js
@@ -1,12 +1,31 @@
-import React, {useParams} from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
-const Company = () => {
-    const {company} = useParams()
-    return (
-        <div>
-            Single Company 
+import "./CompanyCard.css";
+
+/** Show limited information about a company
+ *
+ * Is rendered by CompanyList to show a "card" for each company.
+ *
+ * CompanyList -> CompanyCard
+ */
+
+function CompanyCard({ name, description, logoUrl, handle }) {
+  console.debug("CompanyCard", logoUrl);
+
+  return (
+      <Link className="CompanyCard card" to={`/companies/${handle}`}>
+        <div className="card-body">
+          <h6 className="card-title">
+            {name}
+            {logoUrl && <img src={logoUrl}
+                            alt={name}
+                            className="float-right ml-5" />}
+          </h6>
+          <p><small>{description}</small></p>
         </div>
-    )
+      </Link>
+  );
 }
 
-export default Company
+export default CompanyCard;
